3c89ecf99e768a0886bf555ee9ce9b0a86844a7a
Jeevitha-A <168923526+Jeevitha-A@users.noreply.github.com>
2025-01-09 14:53:40 +0530

    Create IndexSubArraySum.java

---
+++ IndexSubArraySum.java
@@ -0,0 +1,18 @@
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            int sum=0;
+            for(int j=i;j<arr.length;j++){
+                sum=sum+arr[j];
+                if(sum==target){
+                    list.add(i+1);
+                    list.add(j+1);
+                    return list;
+                }
+            }
+        }
+        list.add(-1);
+        return list;
+    }
+}
